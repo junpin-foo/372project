b705f034f64388b4bbf4983bface5d3d753a6f92
kleb-ton <caleb.tongst@gmail.com>
2024-03-09 19:47:05 -0800

    Backend - Added endpoints for authentication - setup basic sessions

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,46 +2,49 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 const Login = () => {
-   const [username, setUsername] = useState('');
-   const [password, setPassword] = useState('');
-   const [error, setError] = useState('');
- 
-   const handleLogin = () => {
-     if (username === 'exampleUser' && password === 'examplePassword') {
-       // Login successful, you can redirect the user to another page or perform further actions here
-       console.log('Login successful');
-     } else {
-       setError('Invalid username or password');
-     }
-   };
- 
-   return (
-     <div>
-       <h2>Login</h2>
-       <div>
-         <label>Username:</label>
-         <input
-           type="text"
-           value={username}
-           onChange={(e) => setUsername(e.target.value)}
-         />
-       </div>
-       <div>
-         <label>Password:</label>
-         <input
-           type="password"
-           value={password}
-           onChange={(e) => setPassword(e.target.value)}
-         />
-       </div>
-       <button onClick={handleLogin}>Login</button>
-       {error && <div>{error}</div>}
-     </div>
-   );
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const backEndpoint = 'http://localhost:3001/'
+
+  const handleLogin = () => {
+    axios.post(backEndpoint + "login", {
+      username: username,
+      password: password
+    })
+    .then(response => {
+
+      console.log('Login successful');
+    })
+    .catch(error => {
+
+      console.error('Login failed', error);
+      setError('Invalid username or password');
+    });
+  };
+
+  return (
+
+    <div>
+        <label>Username:</label>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <label>Password:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
  };
 
-Login.propTypes = {};
-
-Login.defaultProps = {};
 
 export default Login;
