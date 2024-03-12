4cf7523cc5674f6ef015116c27b3477fba7f76f3
Jesse <jessebot.git@gmail.com>
2024-03-12 11:46:28 -0700

    add main dashboard page

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,7 +1,18 @@
 export default function DashboardPage() {
     return (
-        <main>
-            <h1>Dashboard</h1>
+        <main class="container-fluid">
+            <div className="dashboard">
+                <header className="dashboard-header">
+                    <h1 className="text-center">Dashboard</h1>
+                </header>
+
+                <div className="placeholder bg-primary col-12"></div>
+                <div className="placeholder bg-success col-12"></div>
+                <div className="placeholder bg-info col-12"></div>
+                <div className="placeholder col-12"></div>
+                <div className="placeholder bg-warning col-12"></div>
+                <div className="placeholder col-12"></div>
+            </div>
         </main>
     );
 }
